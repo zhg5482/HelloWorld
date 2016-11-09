5585faacea061fab454b4f4e2996ed868bdf468d
zhg5482 <1581992170@qq.com>
2016-11-09 17:09:59 +0800

    update

--- app/src/main/java/main/com/helloworld/NotesActivity.java
+++ app/src/main/java/main/com/helloworld/NotesActivity.java
@@ -1,8 +1,15 @@
 package main.com.helloworld;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +17,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -22,7 +32,15 @@ public class NotesActivity extends Activity {
 
     private Button button_safe;
     private Button button_back;
-    private String fileName = "notes.txt";
+    public static final String DIR = "HelloNotes";
+    public static final String FILENAME = "notes.txt";
+    private EditText notes_txt;
+    private String notes;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +58,9 @@ public class NotesActivity extends Activity {
 
         safeNotes();
         backNotes();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -50,95 +71,101 @@ public class NotesActivity extends Activity {
         button_safe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText notes_txt = (EditText) findViewById(R.id.notes_txt);
-                String notes = notes_txt.getText().toString();
+                notes_txt = (EditText) findViewById(R.id.notes_txt);
+                notes = notes_txt.getText().toString();
 
                 if (notes.equals("")) {
                     Toast.makeText(NotesActivity.this, "文本内容不能为空!", Toast.LENGTH_SHORT).show();
                 } else {
                     Log.i("===", notes);
 
-                    /*try {
-                        FileOutputStream outputStream = openFileOutput(fileName,Activity.MODE_PRIVATE);
-                        outputStream.write(notes.getBytes());
-                        Log.i("+++++++",notes.getBytes()+"");
-                        outputStream.flush();
-                        outputStream.close();
-                        Toast.makeText(NotesActivity.this, "保存成功", Toast.LENGTH_SHORT).show();
-                    }catch (FileNotFoundException e){
-                        e.printStackTrace();
-                    }catch (IOException e){
-                        e.printStackTrace();
-                    }*/
-
-
                     writeSd(notes);
-
-                   /* Log.i("===", getSdCardPath());
-                    try {
-                        File root = Environment.getExternalStorageDirectory();
-                        Log.i("========", root + " 222");
-                        if (isSdCardExist() && root.canWrite()) {
-                            File file = new File(root, fileName);
-
-                            if (!file.exists()) {
-                                file.createNewFile();
-                            }
-
-                            FileOutputStream fos = new FileOutputStream(file);
-                            fos.write(notes.getBytes());
-                            fos.close();
-                        } else {
-                            Toast.makeText(NotesActivity.this, "Can't write.", Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }*/
                 }
-
             }
         });
     }
 
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    public static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
 
-    public static final String DIR = "PictureSiPai";
-    public static final String FILENAME = "photoUpload";
+    public static boolean checkPermission(Activity context, String requestPermission, int requestCode) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int checkCallPhonePermission = ContextCompat.checkSelfPermission(context, requestPermission);
+
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(context,PERMISSIONS_STORAGE, requestCode);
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == 1) {
+                writeSd(notes);
+            }
+
+        } else {
+            Toast.makeText(NotesActivity.this, "请授权!", Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
     // 文件写操作函数
     public void writeSd(String content) {
 
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) { // 如果sdcard存在
-            File file = new File(Environment.getExternalStorageDirectory()
-                    .toString()
-                    + File.separator
-                    + DIR
-                    + File.separator
-                    + FILENAME); // 定义File类对象
+        if (checkPermission(this,READ_EXTERNAL_STORAGE,1)) {
+            Log.i("++++", Environment.getExternalStorageDirectory().canWrite() + "  " + Environment.getExternalStorageDirectory().canRead() );
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) { // 如果sdcard存在
 
-            if (!file.getParentFile().exists()) { // 父文件夹不存在
-                file.getParentFile().mkdirs(); // 创建文件夹
-            }
 
-            PrintStream out = null; // 打印流对象用于输出
+                File file_dir = new File(Environment.getExternalStorageDirectory()
+                        .toString()
+                        + File.separator
+                        + DIR); // 定义File类对象
 
-            try {
-                out = new PrintStream(new FileOutputStream(file, true)); // 追加文件
-                out.println(content);
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (out != null) {
-                    out.close(); // 关闭打印流
+                if (!file_dir.exists()) { // 父文件夹不存在
+                    Log.i("++file_dir.mkdirs()++", file_dir.mkdirs() + "");
+                    // 创建文件夹
                 }
+                Log.i("++file_dir.exists()++", file_dir.exists() + "");
+
+
+                File file = new File(Environment.getExternalStorageDirectory()
+                        .toString()
+                        + File.separator
+                        + DIR
+                        + File.separator
+                        + FILENAME); // 定义File类对象
+                PrintStream out = null; // 打印流对象用于输出
+
+                try {
+                    out = new PrintStream(new FileOutputStream(file, true)); // 追加文件
+                    out.println(content);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (out != null) {
+                        out.close(); // 关闭打印流
+                    }
+                }
+
+                Log.i("haha", file.getAbsolutePath());
+
+                Toast.makeText(NotesActivity.this, "保存成功！", Toast.LENGTH_LONG).show();
+
+            } else { // SDCard不存在，使用Toast提示用户
+
+                Toast.makeText(NotesActivity.this, "保存失败，SD卡不存在！", Toast.LENGTH_LONG).show();
             }
-
-            Log.i("haha",file.getAbsolutePath());
-
-            Toast.makeText(NotesActivity.this, "保存chenggong！", Toast.LENGTH_LONG).show();
-
-        } else { // SDCard不存在，使用Toast提示用户
-
-            Toast.makeText(NotesActivity.this, "保存失败，SD卡不存在！", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -230,7 +257,7 @@ public class NotesActivity extends Activity {
 
     }*/
 
-    public  String readSd() {
+    public String readSd() {
 
         if (Environment.getExternalStorageState().equals(
                 Environment.MEDIA_MOUNTED)) { // 如果sdcard存在
@@ -269,5 +296,41 @@ public class NotesActivity extends Activity {
             Toast.makeText(NotesActivity.this, "读取失败，SD卡不存在！", Toast.LENGTH_LONG).show();
         }
         return null;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Notes Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
